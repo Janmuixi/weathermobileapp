9473e9ee82ffe1a249c8740c14daabc69b938935
janmuixi <janmuixi7@gmail.com>
2019-01-17 20:10:04 +0100

    app working

--- app/src/main/java/e/jan/weatherapp/MainActivity.java
+++ app/src/main/java/e/jan/weatherapp/MainActivity.java
@@ -4,8 +4,16 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -16,21 +24,29 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView webView;
+    EditText ciudad;
+    TextView tiempo;
+    Button consultaBtn;
+
     String data = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AsyncThread thread = new AsyncThread();
+        ciudad = findViewById(R.id.editText);
+        tiempo = findViewById(R.id.textMain);
+        consultaBtn = findViewById(R.id.button);
+        consultaBtn.setText("Send >");
+        tiempo.setText("");
 
-        thread.execute("https://www.google.com");
-
-        webView = findViewById(R.id.webview);
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        consultaBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AsyncThread thread = new AsyncThread();
+                String nombreCiudad = ciudad.getText().toString();
+                thread.execute("https://api.openweathermap.org/data/2.5/weather?q=" + nombreCiudad + "&appid=2ad918c0386450a61010cf6a0ac29ef4");
+            }
+        });
 
     }
 
@@ -63,9 +79,48 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String data) {
-            super.onPostExecute(data);
 
-            webView.loadData(data,"text/html","UTF-8");
+            super.onPostExecute(data);
+            JSONObject weatherObject = null;
+
+            try {
+                weatherObject = new JSONObject(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JSONArray weather = null;
+
+            try {
+                weather = weatherObject.getJSONArray("weather");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JSONObject eachObject = null;
+
+            try {
+                eachObject = weather.getJSONObject(0);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String main = null;
+
+            try {
+                main = eachObject.getString("main");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("JSONObject", String.valueOf(weatherObject));
+            Log.i("JSONArray", String.valueOf(weather));
+            Log.i("JSONString", String.valueOf(eachObject));
+            Log.i("JSONString", String.valueOf(main));
+
+            TextView textView = (TextView) findViewById(R.id.textMain);
+            textView.setText(main);
         }
     }
 }
